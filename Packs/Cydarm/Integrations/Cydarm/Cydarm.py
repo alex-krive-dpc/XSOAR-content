b7bb498860152715eb84d50c85db77015a6329fa
Alex Krive <alex.krive@dpc.vic.gov.au>
2022-02-25 13:25:38 +1100

    Added Cydarm Get-Case and Case Actions

--- Packs/Cydarm/Integrations/Cydarm/Cydarm.py
+++ Packs/Cydarm/Integrations/Cydarm/Cydarm.py
@@ -1,5 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+
+
 import json
 import requests
 import base64
@@ -32,6 +34,39 @@ class Client(BaseClient):
             return [r.status_code, r.headers["Access-Token"]]
         return [r.status_code, r.text]
 
+    def logout(self, token: str):
+        jti = json.loads(base64.b64decode(token.split(".")[1] + "==="))["jti"]
+        url = self.endpoint + "/cydarm-api/auth/session/" + jti
+        headers = {"User-Agent": "DPC XSOAR", "Accept": "application/json", "x-cydarm-authz": token}
+        r = requests.delete(url, headers=headers)
+        return r.status_code == 200
+
+    def getCaseByLocator(self, locator: str):
+        url = self.endpoint + "/cydarm-api/case/locator/" + locator
+        login_status, login_token = self.login()
+        if login_status != 200:
+            return {}
+
+        headers = {"User-Agent": "DPC XSOAR", "Accept": "application/json", "x-cydarm-authz": login_token}
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            self.logout(login_token)
+            return r.json()
+        return {}
+    
+    def getCaseActions(self, uuid: str):
+        url = self.endpoint + f"/cydarm-api/case/{uuid}/action"
+        login_status, login_token = self.login()
+        if login_status != 200:
+            return {}
+        
+        headers = {"User-Agent": "DPC XSOAR", "Accept": "application/json", "x-cydarm-authz": login_token}
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            self.logout(login_token)
+            return r.json()
+        return {}
+
 
 ''' HELPER FUNCTIONS '''
 
@@ -42,9 +77,33 @@ class Client(BaseClient):
 def test_module(client: Client) -> str:
     code, text = client.login()
     if code == 200:
-        return 'ok'
-    return f"Code: {code}: {text}"
+        if client.logout(text):
+            return 'ok'
+        return "Failed to logout??"
+    return f"Failed to login - Code: {code}: {text}"
 
+
+def get_case_by_locator(client: Client, locator: str) -> dict:
+    data = client.getCaseByLocator(locator)
+    values = ["uuid", "locator", "description", "created", "modified", "org", "assignee", "metadata", "status", "severity", "tags", "closed", "acl"]
+    new_data = {key: data[key] for key in values}
+
+    return CommandResults(
+        outputs_prefix='Cydarm.Case',
+        readable_output=new_data,
+        outputs=new_data
+    )
+
+def get_case_actions(client: Client, uuid: str):
+    data = client.getCaseActions(uuid)
+    values = ["actionUuid", "actionDescription", "actionName", "created", "modified", "caseUuid"]
+    new_data = {key: data[key] for key in values}
+
+    return CommandResults(
+        outputs_prefix='Cydarm.CaseActions',
+        readable_output=new_data,
+        outputs=new_data
+    )
 
 ''' MAIN FUNCTION '''
 
@@ -70,6 +129,12 @@ def main() -> None:
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration Test button.
             result = test_module(client)
+            return_results(result)
+        elif demisto.command() == "cydarm-case-by-locator":
+            result = get_case_by_locator(client, demisto.args()["locator"])
+            return_results(result)
+        elif demisto.command() == "cydarm-case-actions":
+            result = get_case_actions(client, demisto.args()["uuid"])
             return_results(result)
 
     # Log exceptions and return errors
